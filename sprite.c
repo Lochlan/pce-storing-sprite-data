b1e7c25fe44e43b6390efe771a53ec9af54fa485
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-01-26 02:02:49 -0800

    Load sprite data from a variable

--- sprite.c
+++ sprite.c
@@ -1,7 +1,25 @@
 #include "huc.h"
 
-#incspr(cigarettes, "graphics/cigarettes.pcx", 0, 0, 2, 1);
-#incpal(cigarettespal, "graphics/cigarettes.pcx");
+/* #incspr(cigarettes, "graphics/cigarettes.pcx", 0, 0, 2, 1);
+#incpal(cigarettespal, "graphics/cigarettes.pcx"); */
+#asm
+    .data
+    _cigarettes:
+        .incspr "graphics/cigarettes.pcx",0,0,2,1
+    _cigarettespal:
+        .incpal "graphics/cigarettes.pcx"
+    .code
+#endasm
+
+
+int sprite_address;
+char sprite_bank;
+int sprite_video_address;
+char sprite_size;
+
+int palette_address;
+int palette_bank;
+
 
 spr_make(spriteno, spritex, spritey, spritepattern, ctrl1, ctrl2, sprpal, sprpri)
 int spriteno, spritex, spritey, spritepattern, ctrl1, ctrl2, sprpal, sprpri;
@@ -26,8 +44,61 @@ main() {
 
     init_satb();
     spr_make(0, 188, 167, 0x5000, FLIP_MAS|SIZE_MAS, NO_FLIP|SZ_32x16, 0, 1);
-    load_palette(16, cigarettespal, 1);
-    load_vram(0x5000, cigarettes, 0x80);
+
+    #asm
+    lda #bank(_cigarettespal)
+    sta _palette_bank
+    lda #low(_cigarettespal)
+    sta _palette_address
+    lda #high(_cigarettespal)
+    sta _palette_address+1
+    #endasm
+
+    /*load_palette(16, cigarettespal, 1);*/
+    #asm
+    __ldwi 16
+    stx _al
+    ;//__farptr _cigarettespal,_bl,_si
+    lda _palette_bank
+    sta _bl
+    lda _palette_address
+    sta _si
+    lda _palette_address+1
+    sta _si+1
+    __ldwi 1
+    stx _cl
+    call _load_palette.3
+    #endasm
+
+    sprite_video_address = 0x5000;
+    #asm
+    lda #bank(_cigarettes)
+    sta _sprite_bank
+    lda #low(_cigarettes)
+    sta _sprite_address
+    lda #high(_cigarettes)
+    sta _sprite_address+1
+    #endasm
+    sprite_size = 0x80;
+
+    /*load_vram(0x5000, cigarettes, 0x80);*/
+    #asm
+    ;//__ldwi $5000
+    ldx _sprite_video_address
+    lda _sprite_video_address+1
+    __stw _di
+    ;//__farptr _cigarettes,_bl,_si
+    lda _sprite_bank
+    sta _bl
+    lda _sprite_address
+    sta _si
+    lda _sprite_address+1
+    sta _si+1
+    __ldwi _sprite_size
+    __stw _cx
+    call _load_vram.3
+    #endasm
+
     satb_update();
     vsync(0);
 }
